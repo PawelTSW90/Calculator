9a0917942860215a2b353b3326565a7c6cc57cb0
Pawel <pawel.dyjak@poczta.onet.pl>
2020-01-20 22:24:32 +0000

    Działający znak wyniku

--- app/src/main/java/com/example/calculator/Magazyn.java
+++ app/src/main/java/com/example/calculator/Magazyn.java
@@ -9,9 +9,31 @@ public class Magazyn {
 
     }
 
+
     String zwrocStringa(){
         return magazyn;
 
 
     }
+
+    int zwrocWynik(){
+        try{
+            int cos = Character.getNumericValue(magazyn.charAt(0));
+            int cos2 = Character.getNumericValue(magazyn.charAt(1));
+            return cos+ cos2;
+
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return 0;
+
+
+
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/calculator/TextFileInput.java
+++ app/src/main/java/com/example/calculator/TextFileInput.java
@@ -13,13 +13,20 @@ public class TextFileInput implements View.OnClickListener {
         this.magazyn = magazyn;
 
 
-
     }
+
 
     @Override
     public void onClick(View v) {
-        magazyn.dodajZnakDoStringa(((Button)v).getText().toString());
-       this.txt.setText(magazyn.zwrocStringa());
+        magazyn.dodajZnakDoStringa(((Button) v).getText().toString());
+        this.txt.setText(magazyn.zwrocStringa());
+        //Log.i("Pawel","id = " + v.getResources().getResourceName(v.getId()));
+
+
+        if(v.getResources().getResourceName(v.getId()).contains("button_result")){
+            int cos = magazyn.zwrocWynik();
+            this.txt.setText(Integer.toString(cos));
+        }
 
 
 
