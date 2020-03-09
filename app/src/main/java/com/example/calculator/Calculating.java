6f05b49b96fa75d8779abbe14d37186f85f25975
Pawel <pawel.dyjak@poczta.onet.pl>
2020-03-09 19:30:16 +0000

    Calculator conflict resolving

--- app/src/main/java/com/example/calculator/Calculating.java
+++ app/src/main/java/com/example/calculator/Calculating.java
@@ -10,38 +10,55 @@ public class Calculating {
         this.storage = storage;
 
         ArrayList znaki = storage.returnWyjscie();
-        ArrayList przetwarzaneZnaki = new ArrayList();
 
 
-        for (int x = 0; x < znaki.size(); x++) {
-            if (isNotInteger(znaki.get(x).toString())) {
+
+        for (int x = 0; x<znaki.size(); x++) {
+            if (!isInteger(znaki.get(x).toString())) {
+
 
                 if (whatSign(znaki.get(x).toString()) == 0) {
                     int sum = (Integer.parseInt(znaki.get(x - 2).toString()) + (Integer.parseInt(znaki.get(x - 1).toString())));
                     znaki.set(x - 2, sum);
-                    znaki.remove(x-1);
-                    znaki.remove(x-1);
-
-                   // Log.i("Pawel", "Znaki" + znaki.toString());
-
-
+                    znaki.remove(x - 1);
+                    znaki.remove(x - 1);
                 }
 
+
+
+
+                else if (whatSign(znaki.get(x).toString()) == 1) {
+                    int minus = (Integer.parseInt(znaki.get(x - 2).toString()) - (Integer.parseInt(znaki.get(x - 1).toString())));
+                    znaki.set(x - 2, minus);
+                    znaki.remove(x - 1);
+                    znaki.remove(x - 1);
+
+                } else if (whatSign(znaki.get(x).toString()) == 2) {
+                    int multiply = (Integer.parseInt(znaki.get(x - 2).toString()) * (Integer.parseInt(znaki.get(x - 1).toString())));
+                    znaki.set(x - 2, multiply);
+                    znaki.remove(x - 1);
+                    znaki.remove(x - 1);
+
+                }
             }
 
         }
 
-        //tmp.deleteCharAt(0).deleteCharAt(tmp.length() - 1);
-        String wynik = przetwarzaneZnaki.toString();
-        return wynik;
+        StringBuilder result = new StringBuilder();
+        result.append(znaki.toString());
+        result.deleteCharAt(0).deleteCharAt(result.length() - 1);
+        storage.storage = result.toString();
 
+        return storage.storage;
     }
 
-    public boolean isNotInteger(String input) {
-
-        if (input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/")) {
+    public boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
             return true;
-        } else return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public int whatSign(String input) {
