bd6eb67b1eb0b32efe3dc19632a5e4ef83c1da8b
Pawel <pawel.dyjak@poczta.onet.pl>
2020-03-22 15:53:11 +0000

    setting cursor properly

--- app/src/main/java/com/example/calculator/TextFileInput.java
+++ app/src/main/java/com/example/calculator/TextFileInput.java
@@ -1,6 +1,5 @@
 package com.example.calculator;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,11 +18,23 @@ public class TextFileInput implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        int kursor = txt.getSelectionEnd();
+        int kursor = txt.getSelectionEnd();                          //initialize cursor position
         storage.addCharToString(((Button) v).getText().toString());
         this.txt.setText(storage.returnString());
-        txt.setSelection(kursor+1);
-        Log.i("proba", storage.storage);
+        txt.setSelection(kursor+1);                                  //place cursor after last added value
+
+
+        if(kursor!=storage.storage.length()-1){                      //If we have more than one value
+             String firstPart = storage.storage.substring(0, kursor);
+             String secondPart = storage.storage.substring(kursor, storage.storage.length()-1); //split storage into two halves,
+             storage.storage = "";                                   //first one: all values before new value
+             storage.storage+=firstPart;                             //second one: all values after new value
+             storage.addCharToString(((Button) v).getText().toString());  //merge new storage from first half, NEW VALUE and second half
+             storage.storage+=secondPart;
+             this.txt.setText(storage.returnString());
+             txt.setSelection(kursor+1);
+
+         }
 
 
 
