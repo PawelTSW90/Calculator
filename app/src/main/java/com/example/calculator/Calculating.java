c9e3638c6cb6d6e018d3adac51afeb12bc635937
Pawel <pawel.dyjak@poczta.onet.pl>
2020-07-07 21:20:36 +0100

    BigDecimal dividing fixed

--- app/src/main/java/com/example/calculator/Calculating.java
+++ app/src/main/java/com/example/calculator/Calculating.java
@@ -1,17 +1,17 @@
 package com.example.calculator;
 
-import android.util.Log;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 class Calculating {
-    final int RESPONSE_OK = 0;
-    final int WRONG_FORMAT = 1;
-    final int RESPONSE_15_DIGITS_LIMIT_REACHED = 2;
-    final int RESPONSE_LIMIT_AFTER_COMMA_REACHED = 3;
-
+    private final int RESPONSE_OK = 0;
+    private final int WRONG_FORMAT = 1;
+    private final int RESPONSE_15_DIGITS_LIMIT_REACHED = 2;
+    private final int RESPONSE_LIMIT_AFTER_COMMA_REACHED = 3;
+    private final int COMMA_SCALE = 10;
+    private final int TOTAL_SCALE = 15;
     //Before calculating, method check if format is correct
     int wrongFormatChecker(String expression) {
 
@@ -22,9 +22,9 @@ class Calculating {
         //regex check if last character is arithmetic symbol
         boolean formatCheck3 = Pattern.matches(".*[+,×÷\\-]$.*", expression);
         //regex check if 15 digit limit is reached
-        boolean fifteenDigitsLimit = Pattern.matches(".*\\d{16,}.*", expression);
+        boolean fifteenDigitsLimit = Pattern.matches(".*\\d{"+(TOTAL_SCALE+1)+",}.*", expression);
         //regex check if 10 digit limit is reached
-        boolean tenDigitsAfterCommaLimit = Pattern.matches(".*,\\d{11,}.*", expression);
+        boolean tenDigitsAfterCommaLimit = Pattern.matches(".*,\\d{"+(COMMA_SCALE+1)+",}.*", expression);
 
         if (formatCheck1 || formatCheck2 || formatCheck3) {
             return WRONG_FORMAT;
@@ -54,32 +54,41 @@ class Calculating {
                     cantCount = true;
                     //adding two values
                 } else if (chars.get(x).equals("+")) {
-                    BigDecimal tmp11 = new BigDecimal(chars.get(x-2));
-                    BigDecimal tmp22 = new BigDecimal(chars.get(x-1));
-                    BigDecimal sum;
-                    sum = tmp11.add(tmp22);
+                    BigDecimal value1, value2, sum;
+                    value1 = new BigDecimal(chars.get(x-2));
+                    value2 = new BigDecimal(chars.get(x-1));
+                    sum = value1.add(value2);
                     chars.set(x - 2, sum.toString());
                     chars.remove(x - 1);
                     chars.remove(x - 1);
                     x = 0;
                     //subtracting two values
                 } else if (Utility.whatSign(chars.get(x)) == 1) {
-                    double minus = (Double.parseDouble(chars.get(x - 2)) - (Double.parseDouble(chars.get(x - 1))));
-                    chars.set(x - 2, Double.toString(minus));
+                    BigDecimal value1, value2, substracting;
+                    value1 = new BigDecimal(chars.get(x-2));
+                    value2 = new BigDecimal(chars.get(x-1));
+                    substracting = value1.subtract(value2);
+                    chars.set(x - 2, substracting.toString());
                     chars.remove(x - 1);
                     chars.remove(x - 1);
                     x = 0;
                     //multiplying two values
                 } else if (Utility.whatSign(chars.get(x)) == 2) {
-                    Double multiply = (Double.parseDouble(chars.get(x - 2)) * (Double.parseDouble(chars.get(x - 1))));
-                    chars.set(x - 2, multiply.toString());
+                    BigDecimal value1, value2, multiplying;
+                    value1 = new BigDecimal(chars.get(x-2));
+                    value2 = new BigDecimal(chars.get(x-1));
+                    multiplying = value1.multiply(value2);
+                    chars.set(x - 2, multiplying.toString());
                     chars.remove(x - 1);
                     chars.remove(x - 1);
                     x = 0;
                     //dividing two values
                 } else if (Utility.whatSign(chars.get(x)) == 3) {
-                    Double divide = (Double.parseDouble(chars.get(x - 2)) / (Double.parseDouble(chars.get(x - 1))));
-                    chars.set(x - 2, divide.toString());
+                    BigDecimal value1, value2, dividing;
+                    value1 = new BigDecimal(chars.get(x-2));
+                    value2 = new BigDecimal(chars.get(x-1));
+                    dividing = value1.divide(value2,COMMA_SCALE, RoundingMode.HALF_UP);
+                    chars.set(x - 2, dividing.stripTrailingZeros().toPlainString());
                     chars.remove(x - 1);
                     chars.remove(x - 1);
                     x = 0;
@@ -97,17 +106,11 @@ class Calculating {
             bg2 = bigDecimalValue.remainder(new BigDecimal(1));
             double bg2toDouble = bg2.doubleValue();
             String bigDecimalToString = bigDecimalValue.toString();
+            storage.setStorage(bigDecimalToString);
 
-            if(bg2toDouble == 0.0){
-                StringBuilder tmp = new StringBuilder(bigDecimalToString);
-                Log.i("proba",""+tmp);
-                tmp = tmp.deleteCharAt(tmp.length()-1);
-                tmp = tmp.deleteCharAt(tmp.length()-1);
-                storage.setStorage(tmp.toString());
-            } else{
+            if(bg2toDouble != 0.0){
                 bigDecimalToString = bigDecimalToString.replace(".", ",");
                 storage.setStorage(bigDecimalToString);
-                //15,63+8 naprawić
 
             }
 
